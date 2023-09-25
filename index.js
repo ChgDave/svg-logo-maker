e2cb3bb67f9c6c2aa804f6a73b3f55ebb331d346
ChgDave <chgdave@gmail.com>
2023-09-24 21:07:31 -0500

    my code

---
+++ index.js
@@ -0,0 +1,10 @@
+// Take command line input to generate object for SVG
+// Text has to be between 1-3 characters
+// Text color can be a color keyword or hexadecimal number
+// Shape is either circle, triangle or square
+// Shape color can be a color keyword or hexadecimal number
+// logo.svg shall be generated after all prompts are completed
+// Output text "Generated logo.svg" should be printed in command line
+
+// Install npm
+// Install jest
