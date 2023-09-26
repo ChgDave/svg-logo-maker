df288fd44d982d99f750eabeaf3e75711cf1b48a
ChgDave <chgdave@gmail.com>
2023-09-25 22:02:28 -0500

    my code

--- index.js
+++ index.js
@@ -10,68 +10,7 @@
 // Install inquirer
 // Install jest
 
-"strict mode";
+const cli = require("./lib/cli.js");
 
-const inquirer = require("inquirer");
-const { Triangle, Circle, Square } = require("./lib/shapes.js");
-const { writeFile } = require("fs/promises");
-
-console.log(Triangle);
-
-inquirer
-  .prompt([
-    {
-      type: "input",
-      message: "Please type in your character:",
-      name: "text",
-    },
-    {
-      type: "input",
-      message:
-        "Please enter the color for the text(can be a color keyword or a hexadecimal number):",
-      name: "textColor",
-    },
-    {
-      type: "list",
-      message: "Please select the shape of your svg:",
-      choices: ["square", "circle", "triangle"],
-      name: "shape",
-    },
-    {
-      type: "input",
-      message:
-        "Please enter the color for the shape(can be a color keyword or a hexadecimal number):",
-      name: "shapeColor",
-    },
-  ])
-  .then((response) => {
-    console.log(response);
-    const { text, textColor, shape, shapeColor } = response;
-    let svg;
-    console.log(shape);
-    // Use this input to generate the svg file
-    switch (shape) {
-      case "triangle":
-        const triangle = new Triangle(shapeColor, text, textColor);
-        svg = triangle.render();
-        console.log(svg);
-        break;
-      case "square":
-        const square = new Square(shapeColor, text, textColor);
-        svg = square.render();
-        console.log(svg);
-        break;
-      case "circle":
-        const circle = new Circle(shapeColor, text, textColor);
-        svg = circle.render();
-        console.log(svg);
-        break;
-    }
-    return { shape, svg };
-  })
-  .then((data) => {
-    console.log(data);
-    writeFile(`./examples/${data.shape}.svg`, data.svg);
-  })
-  .then(() => console.log("Generated logo.svg"))
-  .catch((err) => console.error(err));
+// console.log(cli);
+cli();
